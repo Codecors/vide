7d64d50f398e8ecf43cad592e747c4233213ea20
Codecors <info@stylehairs.tk>
2017-02-13 20:51:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Get the file
-$content = file_get_contents("http://www.google.co.in/intl/en_com/images/srpr/logo1w.png");
+$content = file_get_contents("https://p-def1.pcloud.com/cfZAEHSMkZTdeXMkZTvUxZZeAYf67ZQ5ZZC5FZZQ4mROZ47ZbVZo7ZxVZFZnXZ2VZkXZXkZDXZdZ3ZD7Z3kZzucjSqm5YY8Gk7hkqXmmPBslCA37/Ae%20Dil%20Hai%20Mushkil%20%282016%29%20DVDRip%20720p%20Hindi%20Movie%20-%201.1%20GB%20-%20Drkoul-%20%28PerfectHDmovies.pw%29.mp4");
 //Store in the filesystem.
 $fp = fopen("image.jpg", "w");
 fwrite($fp, $content);
