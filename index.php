bc4e1782d4b7b6259c7fb92ed124e08ed8b10125
Codecors <info@stylehairs.tk>
2017-02-13 20:49:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //Get the file
 $content = file_get_contents("http://www.google.co.in/intl/en_com/images/srpr/logo1w.png");
 //Store in the filesystem.
-$fp = fopen("/location/to/save/image.jpg", "w");
+$fp = fopen("image.jpg", "w");
 fwrite($fp, $content);
 fclose($fp);
 ?>
